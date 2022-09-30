aefc41c2128b46d232ebf59b5f9b7c7f5ef78a1c
Demian Shtepa <demian.s@myrunway.co.za>
2022-09-30 22:48:30 +0300

    flake8

--- items/models.py
+++ items/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.urls import reverse
 import uuid
@@ -11,8 +13,44 @@ class Item(models.Model):
         return reverse('item-detail', args=[str(self.id)])
 
     id = models.UUIDField(primary_key=True,
-                          default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+                          default=uuid.uuid4, help_text="Unique ID")
     name = models.CharField(max_length=255, help_text='Item name')
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    create_at = models.DateTimeField
-    update_at = models.DateTimeField
+    description = models.CharField(max_length=255, default='Description', help_text='Item description')
+    image = models.ImageField(upload_to='Images', default='default.jpg')
+    create_at = models.DateTimeField(default=datetime.now(), blank=True)
+    update_at = models.DateTimeField(default=datetime.now(), blank=True)
+
+
+class Category(models.Model):
+    def __str__(self):
+        return self.name
+
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4, help_text="Unique ID")
+    name = models.CharField(max_length=255, help_text='Category name')
+    description = models.CharField(max_length=255, default='Description', help_text='Category description')
+    image = models.ImageField(upload_to='Images', default='default.jpg')
+    create_at = models.DateTimeField(default=datetime.now(), blank=True)
+    update_at = models.DateTimeField(default=datetime.now(), blank=True)
+
+
+class Product(models.Model):
+    def __str__(self):
+        return self.id
+
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4, help_text="Unique ID")
+    price = models.PositiveIntegerField(default=0)
+    sku = models.CharField(max_length=255)
+
+
+class Discount(models.Model):
+    def __str__(self):
+        return self.id
+
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4, help_text="Unique ID")
+    amount = models.PositiveIntegerField(default=1)
+    code = models.CharField(max_length=255, default='Code')
+    is_active = models.BooleanField(default=True)
+    discount_type = models.BooleanField(default=0)
